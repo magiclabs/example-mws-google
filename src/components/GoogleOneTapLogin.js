658d48a195653af9354ec158515c0b1b3f5b6033
Angel Hernandez <angel@magic.link>
2023-05-10 12:54:09 -0400

    add console log of DIDT upon successful Magic login

--- src/components/GoogleOneTapLogin.js
+++ src/components/GoogleOneTapLogin.js
@@ -26,6 +26,7 @@ export default function GoogleOneTapLogin() {
       });
 
       if (did) {
+        console.log("DID Token:", did);
         const magicUser = await magic.user.getMetadata();
 
         // parse info from id token
@@ -55,56 +56,84 @@ export default function GoogleOneTapLogin() {
     setDisabled(true);
 
     try {
-      google.accounts.id.initialize({
-        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        callback: handleResponse,
-      });
+      // google.accounts.id.initialize({
+      //   client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      //   callback: handleResponse,
+      // });
+      const token = {
+        credential: {
+          iss: "https://accounts.google.com",
+          nbf: 1680552233,
+          aud: "371892832861-v0ip6hnmf5auim0ih8ul07c5e24njm75.apps.googleusercontent.com",
+          sub: "103652339078345098862",
+          hd: "magic.link",
+          email: "angel@magic.link",
+          email_verified: true,
+          azp: "371892832861-v0ip6hnmf5auim0ih8ul07c5e24njm75.apps.googleusercontent.com",
+          name: "Angel Hernandez",
+          picture:
+            "https://lh3.googleusercontent.com/a/AGNmyxat829A_P3pKfR4EM_8ceABw77L3rnh1_sPgd1U=s96-c",
+          given_name: "Angel",
+          family_name: "Hernandez",
+          iat: 1680552533,
+          exp: 1680556133,
+          jti: "2cd400ff3f9948e5a0982e28527236db264162ed",
+          wallet: {
+            issuer: "did:ethr:0xB9A8a7cD23C410cE66f21368a375E3B405e91264",
+            publicAddress: "0xb9a8a7cd23c410ce66f21368a375e3b405e91264",
+            email: null,
+            isMfaEnabled: false,
+            phoneNumber: null,
+          },
+        },
+      };
 
-      google.accounts.id.prompt((notification) => {
-        if (notification.isNotDisplayed()) {
-          console.log(
-            "%c Google notification not displayed for reason:",
-            "color: orange; font-weight: bold;",
-            notification.getNotDisplayedReason()
-          );
+      handleResponse(token);
+      // google.accounts.id.prompt((notification) => {
+      //   if (notification.isNotDisplayed()) {
+      //     console.log(
+      //       "%c Google notification not displayed for reason:",
+      //       "color: orange; font-weight: bold;",
+      //       notification.getNotDisplayedReason()
+      //     );
 
-          setDisabled(false);
+      //     setDisabled(false);
 
-          throw new Error("Try clearing cookies and then login again.");
-        }
+      //     throw new Error("Try clearing cookies and then login again.");
+      //   }
 
-        if (notification.isSkippedMoment()) {
-          console.log(
-            "%c Google notification skipped for reason:",
-            "color: orange; font-weight: bold;",
-            notification.getSkippedReason()
-          );
+      //   if (notification.isSkippedMoment()) {
+      //     console.log(
+      //       "%c Google notification skipped for reason:",
+      //       "color: orange; font-weight: bold;",
+      //       notification.getSkippedReason()
+      //     );
 
-          setDisabled(false);
+      //     setDisabled(false);
 
-          throw new Error("Try clearing cookies and then login again.");
-        }
+      //     throw new Error("Try clearing cookies and then login again.");
+      //   }
 
-        if (notification.isDismissedMoment()) {
-          console.log(
-            "%c Google notification dismissed for reason:",
-            "color: orange; font-weight: bold;",
-            notification.getDismissedReason()
-          );
+      //   if (notification.isDismissedMoment()) {
+      //     console.log(
+      //       "%c Google notification dismissed for reason:",
+      //       "color: orange; font-weight: bold;",
+      //       notification.getDismissedReason()
+      //     );
 
-          setDisabled(false);
-        }
+      //     setDisabled(false);
+      //   }
 
-        if (notification.isDisplayed()) {
-          console.log(
-            "%c Google notification displayed with moment type:",
-            "color: orange; font-weight: bold;",
-            notification.getMomentType()
-          );
+      //   if (notification.isDisplayed()) {
+      //     console.log(
+      //       "%c Google notification displayed with moment type:",
+      //       "color: orange; font-weight: bold;",
+      //       notification.getMomentType()
+      //     );
 
-          console.log("is display moment:", notification.isDisplayMoment());
-        }
-      });
+      //     console.log("is display moment:", notification.isDisplayMoment());
+      //   }
+      // });
     } catch (error) {
       setUser();
       console.error(error);
